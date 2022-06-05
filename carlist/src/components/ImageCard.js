5eed9894d78f3a0b010b792664e7654f33b2e139
Nenad <68515352+Nenad95M@users.noreply.github.com>
2022-06-05 22:16:59 +0200

    q

--- carlist/src/components/ImageCard.js
+++ carlist/src/components/ImageCard.js
@@ -1,13 +1,33 @@
 import React from "react";
 
-class ImageCard extends React.Component{
-    render(){
-        const {description, urls}=this.props.image;
-        return (
-            <div>
-               <img src={urls.regular} alt={description} />
+class ImageCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={spans:0};
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
+    };
+    setSpans=()=>{
+       const height =this.imageRef.current.clientHeight;
+       const spans=Math.ceil(height/10);
+       this.setState({spans})
+    }
+
+    render() {
+        const {description, urls} = this.props.image;
+        return(
+            <div style={{gridRowEnd:`span ${this.state.spans}`}}>
+                <img 
+                    ref={this.imageRef}
+                    src={urls.regular} 
+                    alt={description} 
+                />
             </div>
         )
     }
 }
+
 export default ImageCard;
